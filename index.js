50d8d92a8c4db605bcc2507cf462523f56fe954a
gmp <georges@leaneous.com>
2015-09-23 13:29:41 +0200

    init

---
+++ index.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var csv = require('csv-parser');
+
+app.set('view engine', 'ejs');
+
+app.use(express.static("views"));
+
+// Get csv data
+
+var data = {
+    labels : ["January","February","March","April","May","June"],
+        datasets : [
+    {
+        fillColor : "rgba(172,194,132,0)",
+        strokeColor : "#ACC26D",
+        pointColor : "#fff",
+        pointStrokeColor : "#9DB86D",
+        data : [203,156,99,251,305,247]
+    }
+]
+};
+
+app.get('/', function(req, res) {
+
+    var labels = [];
+    var values = [];
+
+    fs.createReadStream('data.csv')
+        .pipe(csv())
+        .on('data', function(data) {
+            console.log('row', data);
+            labels.push(data.km.toString());
+            values.push(data.price);
+        })
+        .on('end', function(){
+            data.labels = labels.reverse();
+            data.datasets[0].data = values.reverse();
+            res.render('index', { data: data });
+        });
+});
+
+app.listen(4200);
+
+console.log('Server running at http://127.0.0.1:4200/');
---
+++ views/index.js
@@ -0,0 +1,21 @@
+(function(){
+    'use strict';
+
+    //var buyerData = {
+    //    labels : ["January","February","March","April","May","June"],
+    //    datasets : [
+    //        {
+    //            fillColor : "rgba(172,194,132,0)",
+    //            strokeColor : "#ACC26D",
+    //            pointColor : "#fff",
+    //            pointStrokeColor : "#9DB86D",
+    //            data : [203,156,99,251,305,247]
+    //        }
+    //    ]
+    //}
+    //// Get the context of the canvas element we want to select
+    //var countries= document.getElementById("graph").getContext("2d");
+    //new Chart(countries).Line(buyerData, {
+    //    responsive: true
+    //});
+})();
